a63509aeae38a5976566ebd09884723c5e63eed8
Adam Tengler <atengler@mirantis.com>
2017-10-26 13:08:13 +0200

    Filtering for topology vizualizations

--- kqueen_ui/auth.py
+++ kqueen_ui/auth.py
@@ -1,10 +1,17 @@
 from kqueen_ui.api import KQueenAPIClient
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 def authenticate(username, password):
     client = KQueenAPIClient(username=username, password=password)
     token = client.token
     if token:
-        users = client.user.list()
-        return ([u for u in users if u['username'] == username][0], token)
+        _users = client.user.list()
+        users = _users.data
+        if not _users.error:
+            return ([u for u in users if u['username'] == username][0], token)
+        logger.error(_users.error)
     return ({}, '')
