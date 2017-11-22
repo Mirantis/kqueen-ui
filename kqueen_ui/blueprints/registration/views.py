23e0318be05189be336c50e4379bde0c774df33e
Adam Tengler <atengler@mirantis.com>
2017-11-22 19:52:55 +0100

    Add mail manager and email verification process

--- kqueen_ui/blueprints/registration/views.py
+++ kqueen_ui/blueprints/registration/views.py
@@ -1,13 +1,16 @@
 from flask import (current_app as app, abort, Blueprint, flash, jsonify, redirect,
                    render_template, request, session, url_for)
+from flask_mail import Mail, Message
 from kqueen_ui.api import get_kqueen_client
 from slugify import slugify
 
 from .forms import UserRegistrationForm
+from .utils import confirm_token, generate_confirmation_token
 
 import logging
 
 logger = logging.getLogger(__name__)
+mail = Mail()
 
 registration = Blueprint('registration', __name__, template_folder='templates')
 
@@ -37,7 +40,7 @@ def register():
 
             organization_id = response.data['id']
         except Exception as e:
-            logger.error('user_create view: {}'.format(repr(e)))
+            logger.error('register view: {}'.format(repr(e)))
             flash('Could not create organization.', 'danger')
             return render_template('registration/register.html', form=form)
 
@@ -57,12 +60,55 @@ def register():
                 client.organization.delete(organization_id)
                 return render_template('registration/register.html', form=form)
         except Exception as e:
-            logger.error('user_create view: {}'.format(repr(e)))
+            logger.error('register view: {}'.format(repr(e)))
             client.organization.delete(organization_id)
             flash('Could not create user.', 'danger')
             return render_template('registration/register.html', form=form)
 
         flash('Registration successful. Check your e-mail for the activation link!', 'success')
+
+        # Init mail handler
+        mail.init_app(app)
+        token = generate_confirmation_token(user['email'])
+        html = render_template('registration/email/verify_email.html', token=token)
+        msg = Message(
+            '[KQueen] E-mail verification',
+            recipients=[user['email']],
+            html=html
+        )
+        try:
+            mail.send(msg)
+        except Exception as e:
+            logger.error('register view: {}'.format(repr(e)))
+            flash('Could not send verification e-mail, please try again later.', 'danger')
+            return render_template('registration/register.html', form=form)
+
         return redirect(url_for('ui.login'))
 
     return render_template('registration/register.html', form=form)
+
+
+@registration.route('/verify/<token>')
+def verify_email(token):
+    email = confirm_token(token)
+    if not email:
+        flash('The confirmation link is invalid or has expired.', 'danger')
+        return redirect(url_for('ui.index'))
+
+    client = _get_client()
+    _users = client.user.list()
+    users = _users.data
+
+    # TODO: this logic realies heavily on unique emails, this is not the case on backend right now
+    filtered = [u for u in users if u.get('email', None) == email]
+    if filtered:
+        user = filtered[0]
+        if user.get('active', None):
+            flash('Account already verified. Please login.', 'success')
+        else:
+            user['active'] = True
+            client.user.update(user['id'], user)
+            flash('You have confirmed your account. Thanks!', 'success')
+    else:
+        flash('No user found based on given e-mail.', 'danger')
+    return redirect(url_for('ui.index'))
--- kqueen_ui/config/dev.py
+++ kqueen_ui/config/dev.py
@@ -1,7 +1,13 @@
 DEBUG = True
 
+# External endpoint
+SERVER_NAME = 'localhost:8000'
+
 # App secret
 SECRET_KEY = 'secret'
+SECURITY_PASSWORD_SALT = 'secret_salt'
+
+# KQueen API
 KQUEEN_API_URL = 'http://localhost:5000/api/v1/'
 KQUEEN_AUTH_URL = 'http://localhost:5000/api/v1/auth'
 
@@ -9,9 +15,21 @@ KQUEEN_AUTH_URL = 'http://localhost:5000/api/v1/auth'
 BABEL_DEFAULT_TIMEZONE = 'UTC'
 BABEL_DEFAULT_LOCALE = 'en'
 
+# Registration
 ENABLE_PUBLIC_REGISTRATION = True
 KQUEEN_SERVICE_USER_NAME = 'admin'
 KQUEEN_SERVICE_USER_PASSWORD = 'default'
+
+# Mail
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+MAIL_USE_TLS = False
+MAIL_USE_SSL = False
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+MAIL_DEFAULT_SENDER = 'noreply@kqueen.com'
+MAIL_MAX_EMAILS = None
+MAIL_ASCII_ATTACHMENTS = False
 
 # Cluster statuses
 CLUSTER_ERROR_STATE = 'Error'
