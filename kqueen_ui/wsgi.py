2226e97ad28ace9d72dcf89748adbac8951d4938
Adam Tengler <atengler@mirantis.com>
2017-12-18 17:28:24 +0100

    Proxy hotfix

--- kqueen_ui/wsgi.py
+++ kqueen_ui/wsgi.py
@@ -1,6 +1,8 @@
 """WGSI module to run application using Gunicorn server."""
 
 from kqueen_ui.server import app as application
+from werkzeug.contrib.fixers import ProxyFix
 
 if __name__ == '__main__':
+    application.wsgi_application = ProxyFix(application.wsgi_application)
     application.run()
