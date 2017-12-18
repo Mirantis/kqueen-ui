1d5bc25aca6b5129efa3d7dcb9618cc67b944880
Tomáš Kukrál <tomkukral@users.noreply.github.com>
2017-12-18 16:21:53 +0100

    copy static files before starcopy static files before startt

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# STATIC_DIR - set this to copy static files before start
+
+if [ ! -z "${STATIC_DIR}" ]; then
+	echo "Running in Docker compose mode"
+	mkdir -p "${STATIC_DIR}"
+	cp -vr /code/kqueen_ui/asset/static/* "${STATIC_DIR}"
+fi
+
 exec gunicorn --bind 0.0.0.0:5080 --workers 4 kqueen_ui.wsgi
