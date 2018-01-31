69d237e9f5492f2c3d1290464330dd2fee6c5c2a
Adam Tengler <atengler@mirantis.com>
2018-01-31 16:31:42 +0100

    Add wrapper to check for superadmin role on user

--- kqueen_ui/utils/wrappers.py
+++ kqueen_ui/utils/wrappers.py
@@ -12,3 +12,14 @@ def login_required(f):
             return redirect(url_for('ui.login', next=request.url))
         return f(*args, **kwargs)
     return decorated_function
+
+
+def superadmin_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get('user', {}).get('id', None):
+            return redirect(url_for('ui.login', next=request.url))
+        elif session['user'].get('role', 'Member') != 'superadmin':
+            return redirect(url_for('ui.login', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
