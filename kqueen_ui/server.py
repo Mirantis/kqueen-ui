5a23c20f51345fab681461fb7568eb75e26cf4c9
Adam Tengler <atengler@mirantis.com>
2018-01-31 16:31:25 +0100

    Add Manager blueprint

--- kqueen_ui/server.py
+++ kqueen_ui/server.py
@@ -1,6 +1,7 @@
 from .config import current_config
 
 from flask import Flask, redirect, request, url_for
+from kqueen_ui.blueprints.manager.views import manager
 from kqueen_ui.blueprints.registration.views import registration
 from kqueen_ui.blueprints.ui.views import ui
 from kqueen_ui.exceptions import KQueenAPIException
@@ -13,9 +14,9 @@ logger = logging.getLogger(__name__)
 
 def create_app(config_file=None):
     app = Flask(__name__, static_folder='./asset/static')
-
-    app.register_blueprint(ui, url_prefix='/ui')
+    app.register_blueprint(manager, url_prefix='/manager')
     app.register_blueprint(registration, url_prefix='/registration')
+    app.register_blueprint(ui, url_prefix='/ui')
 
     # load configuration
     config = current_config(config_file)
