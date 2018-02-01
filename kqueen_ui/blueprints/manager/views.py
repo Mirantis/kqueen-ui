09d6f262b574315cd6ea9866ed8471267c51fdd7
Adam Tengler <atengler@mirantis.com>
2018-02-01 16:59:34 +0100

    Organization detail and MemberCreate views

--- kqueen_ui/blueprints/manager/views.py
+++ kqueen_ui/blueprints/manager/views.py
@@ -1,8 +1,11 @@
-from .forms import OrganizationCreateForm
+from .forms import MemberCreateForm, OrganizationCreateForm
 from datetime import datetime
-from flask import Blueprint, flash, jsonify, redirect, render_template, request, session, url_for
+from flask import Blueprint, current_app as app, flash, jsonify, redirect, render_template, request, session, url_for
 from flask_babel import format_datetime
-from kqueen_ui.blueprints.ui.utils import sanitize_resource_metadata
+from flask_mail import Mail, Message
+from kqueen_ui.api import get_kqueen_client
+from kqueen_ui.auth import generate_confirmation_token
+from kqueen_ui.blueprints.ui.utils import generate_password, sanitize_resource_metadata
 from kqueen_ui.generic_views import KQueenView
 from kqueen_ui.utils.wrappers import superadmin_required
 from slugify import slugify
@@ -10,8 +13,38 @@ from slugify import slugify
 import logging
 
 logger = logging.getLogger(__name__)
+mail = Mail()
 manager = Blueprint('manager', __name__, template_folder='templates')
 
+
+##############
+# Interceptors
+##############
+
+@manager.before_request
+def test_token():
+    if session.get('user', None) and not app.testing:
+        client = get_kqueen_client(token=session['user']['token'])
+        organization_id = session['user']['organization']['id']
+        response = client.organization.policy(organization_id)
+        if response.status == 401:
+            flash('Session expired, please log in again.', 'warning')
+            del session['user']
+            if 'policy' in session:
+                del session['policy']
+        elif response.status == -1:
+            flash('Backend is unavailable at this time, please try again later.', 'danger')
+            del session['user']
+            if 'policy' in session:
+                del session['policy']
+        policy = response.data
+        if policy and isinstance(policy, dict):
+            session['policy'] = policy
+
+
+###############
+# General Views
+###############
 
 class Overview(KQueenView):
     decorators = [superadmin_required]
@@ -88,8 +121,88 @@ class OrganizationCreate(KQueenView):
         return render_template('manager/organization_create.html', form=form)
 
 
+class OrganizationDetail(KQueenView):
+    decorators = [superadmin_required]
+    methods = ['GET']
+
+    def handle(self, organization_id):
+        # session data
+        user_id = session['user']['id']
+        # backend resources
+        organization = self.kqueen_request('organization', 'get', fnkwargs={'uuid': organization_id})
+        users = self.kqueen_request('user', 'list')
+        members = [
+            u
+            for u
+            in users
+            if u['organization']['id'] == organization_id and u['id'] != user_id
+        ]
+        # sort members by date
+        members.sort(key=lambda k: (k['created_at'], k['username']))
+
+        # Patch members until we actually have these data for realsies
+        for member in members:
+            member['state'] = 'Active' if member['active'] else 'Disabled'
+            if 'email' not in member:
+                member['email'] = '-'
+            if 'created_at' in member:
+                member['created_at'] = format_datetime(member['created_at'])
+
+        return render_template('manager/organization_detail.html',
+                               organization=organization,
+                               members=members)
+
+
+class MemberCreate(KQueenView):
+    methods = ['GET', 'POST']
+
+    def handle(self, organization_id):
+        form = MemberCreateForm()
+        if form.validate_on_submit():
+            organization = self.kqueen_request('organization', 'get', fnkwargs={'uuid': organization_id})
+            user_kw = {
+                'username': form.email.data,
+                'email': form.email.data,
+                'password': generate_password(),
+                'organization': 'Organization:{}'.format(organization_id),
+                'created_at': datetime.utcnow(),
+                'role': form.role.data,
+                'active': True,
+                'metadata': {}
+            }
+            user = self.kqueen_request('user', 'create', fnkwargs={'payload': user_kw})
+
+            # Init mail handler
+            mail.init_app(app)
+            token = generate_confirmation_token(user['email'])
+            html = render_template(
+                'ui/email/user_invitation.html',
+                username=user['username'],
+                token=token,
+                organization=user['organization']['name']
+            )
+            msg = Message(
+                '[KQueen] Organization invitation',
+                recipients=[user['email']],
+                html=html
+            )
+            try:
+                mail.send(msg)
+            except Exception as e:
+                self.logger('error', repr(e))
+                self.kqueen_request('user', 'delete', fnargs={'uuid', user['id']})
+                flash('Could not send invitation e-mail, please try again later.', 'danger')
+                return render_template('manager/member_create.html', form=form)
+
+            flash('Member {} successfully added.'.format(user['username']), 'success')
+            return redirect(url_for('manager.organization_detail', organization_id=organization_id))
+        return render_template('manager/member_create.html', form=form)
+
+
 manager.add_url_rule('/', view_func=Overview.as_view('overview'))
 manager.add_url_rule('/data/clusters', view_func=DataClusters.as_view('data_clusters'))
 manager.add_url_rule('/data/provisioners', view_func=DataProvisioners.as_view('data_provisioners'))
 manager.add_url_rule('/organization/create', view_func=OrganizationCreate.as_view('organization_create'))
 manager.add_url_rule('/organization/<organization_id>/delete', view_func=OrganizationDelete.as_view('organization_delete'))
+manager.add_url_rule('/organization/<organization_id>/detail', view_func=OrganizationDetail.as_view('organization_detail'))
+manager.add_url_rule('/organization/<organization_id>/member/create', view_func=MemberCreate.as_view('member_create'))
