e5eabae2fd4e4010095c1fb9ccc7a2cf6d220053
Adam Tengler <atengler@mirantis.com>
2018-03-21 15:48:20 +0100

    Add AUTH_OPTIONS CSRF options to test config

--- kqueen_ui/config/test.py
+++ kqueen_ui/config/test.py
@@ -9,4 +9,20 @@ class Config(BaseConfig):
     SECRET_KEY = 'secret'
     SECURITY_PASSWORD_SALT = 'secret_salt'
 
+    # CSRF settings
+    CSRF_ENABLED = False
+    WTF_CSRF_ENABLED = False
+
     ENABLE_PUBLIC_REGISTRATION = True
+
+    # Authentication choices
+    AUTH_OPTIONS = {
+        'local': {
+            'label': 'Local',
+            'notify': True
+        },
+        'ldap': {
+            'label': 'LDAP',
+            'notify': False
+        }
+    }
